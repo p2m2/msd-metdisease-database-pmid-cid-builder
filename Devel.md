22243a630496be1bd5007822c3200c00861bee86
Olivier Filangi <you@example.com>
2022-06-16 11:03:10 +0200

    save work to use sparqlify method

--- Devel.md
+++ Devel.md
@@ -1,7 +1,6 @@
 
-## Install Spark/Hadoop 
+## Install Spark
 
-- Hadoop : [3.3.0](https://archive.apache.org/dist/hadoop/common/hadoop-3.3.0/hadoop-3.3.0.tar.gz) 
 - Spark : [3.1.2](https://dlcdn.apache.org/spark/spark-3.1.3/spark-3.1.3-bin-hadoop3.2.tgz) 
 
 ``` 
@@ -10,27 +9,15 @@ sudo mv spark-3.1.3-bin-hadoop3.2 /usr/local/share/
 sudo ln -s /usr/local/share/spark-3.1.3-bin-hadoop3.2 /usr/local/share/spark
 ```
 
-``` 
-tar xvf hadoop-3.3.0.tar.gz
-sudo mv hadoop-3.3.0 /usr/local/share/
-sudo ln -s /usr/local/share/hadoop-3.3.0 /usr/local/share/hadoop
-``` 
-
-### set xml config file 
-https://hadoop.apache.org/docs/stable/hadoop-project-dist/hadoop-common/SingleCluster.html
-
-
-```
-export SPARK_HOME=/usr/local/share/spark
-export HADOOP_HOME=/usr/local/share/hadoop
-```
-
-`/usr/local/share/spark/bin/spark-submit --class fr.inrae.msd.rdf.PmidCidBuilder --executor-memory 1G --num-executors 1 target/scala-2.12/msd-metdisease-database-pmid-cid-builder.jar --versionMsd "test" -d ./rdf`
-
 ## addd Sansa with spark-submit Dependencies
 
 ```sh
-/usr/local/share/spark/bin/spark-submit --class fr.inrae.msd.rdf.PmidCidBuilder --executor-memory 1G --num-executors 1 --jars /media/olivier/hdd-local/workspace/INRAE/P2M2/msd-metdisease-database-pmid-cid-builder/sansa-ml-spark_2.12-0.8.0-RC3-SNAPSHOT-jar-with-dependencies.jar target/scala-2.12/msd-metdisease-database-pmid-cid-builder.jar -d ./rdf
+/usr/local/share/spark/bin/spark-submit \
+   --class fr.inrae.msd.rdf.PmidCidBuilder \
+   --executor-memory 1G \
+   --num-executors 1 \
+   --jars ./sansa-ml-spark_2.12-0.8.0-RC3-SNAPSHOT-jar-with-dependencies.jar \
+    target/scala-2.12/msd-metdisease-database-pmid-cid-builder.jar -d ./rdf
 ```
 
 ## Msd Datalake
--- src/main/scala/fr/inrae/msd/rdf/PmidCidWork.scala
+++ src/main/scala/fr/inrae/msd/rdf/PmidCidWork.scala
@@ -2,24 +2,41 @@ package fr.inrae.msd.rdf
 
 import net.sansa_stack.ml.spark.featureExtraction.SparqlFrame
 import net.sansa_stack.query.spark.SPARQLEngine
+import net.sansa_stack.query.spark.api.domain.ResultSetSpark
+import net.sansa_stack.query.spark.sparqlify.QueryEngineFactorySparqlify
 import net.sansa_stack.rdf.spark.io.RDFReader
 import net.sansa_stack.rdf.spark.model.TripleOperations
 import org.apache.jena.riot.Lang
+import org.apache.jena.sparql.core.Var
+import org.apache.jena.sparql.engine.binding.Binding
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 
 case object PmidCidWork {
   def getPMIDListFromReference(spark : SparkSession,referencePath: String): Seq[String] = {
     val lang = Lang.TURTLE
 
-    val triplesDataset = spark.rdf(lang)(referencePath).toDS()
+    val triples = spark.rdf(lang)(referencePath)
+    val triplesDataset = triples.toDS()
     val queryString = "select ?s where { ?s <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://purl.org/spar/fabio/JournalArticle> . }"
 
-    val sparqlFrame = new SparqlFrame()
+    val sparqlFrame =
+      new SparqlFrame()
       .setSparqlQuery(queryString)
       .setQueryExcecutionEngine(SPARQLEngine.Sparqlify)
 
-    val res = sparqlFrame.transform(triplesDataset).collect()
-    res.map(row => row.toString())
+    val queryEngineFactory = new QueryEngineFactorySparqlify(spark)
+    val qef1 = queryEngineFactory.create(triples)
+    val qe = qef1.createQueryExecution(queryString)
+    val result: ResultSetSpark = qe.execSelectSpark()
+    val resultBindings: RDD[Binding] = result.getBindings // the bindings, i.e. mappings from vars to RDF resources
+    val resultVars: Seq[Var] = result.getResultVars
+
+    println("==================  resultVars ========================")
+    println(resultVars.mkString(","))
+    resultVars.map( _.toString)
+   // val res = sparqlFrame.transform(triplesDataset).collect()
+   // res.map(row => row.toString())
    // val str : String = spark.read.text(referencePath).collect().map(row => row.mkString("")).mkString("\n")
   }
 
